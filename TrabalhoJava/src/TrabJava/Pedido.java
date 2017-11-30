986ad783ccbb1a16ce687d0218211af0c3c22baa
Jorge Madson <filholouco@gmail.com>
2017-11-30 15:07:16 -0200

    Inclusão da polimorfia para pizza

--- TrabalhoJava/src/TrabJava/Pedido.java
+++ TrabalhoJava/src/TrabJava/Pedido.java
@@ -5,6 +5,13 @@
  */
 package TrabJava;
 
+import TrabJava.Pizzas.Circular;
+import TrabJava.Pizzas.Forma;
+import TrabJava.Pizzas.Quadrado;
+import TrabJava.Pizzas.Triangulo;
+import java.util.Scanner;
+import jdk.nashorn.internal.parser.Lexer;
+
 /**
  *
  * @author Jorge
@@ -16,5 +23,26 @@ public class Pedido {
 	private String nome;
 
 	private String status;
+        
+        public static void main(String[] args) {
+        //Teste polimorfia
+            Forma f;
+            System.out.println("Escolha uma pizza:\n1- Pizza Quadrada\n2- Pizza Circular\n3- Pizza Triangulo");
+            Scanner sc = new Scanner(System.in);
+            int escolha =sc.nextInt();
+            if(escolha==1){
+                f = new Quadrado();
+                System.out.println("Pizza escolhida: Quadrada ");
+            }
+            else if(escolha==2){
+                f = new Circular();
+                System.out.println("Pizza escolhida: Circular");
+            }
+            else
+            {
+                f = new Triangulo();
+                System.out.println("Pizza escolhida: Triangulo");
+            }
+    }
 
 }
